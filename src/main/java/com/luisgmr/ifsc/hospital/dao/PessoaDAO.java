d2fa0c2f2f707a7b393463d80a7dac177583380c
Luis Gustavo Miranda <luisgmr.privado@hotmail.com>
2025-04-09 23:20:13 -0300

    add hibernateutils

--- src/main/java/com/luisgmr/ifsc/hospital/dao/PessoaDAO.java
+++ src/main/java/com/luisgmr/ifsc/hospital/dao/PessoaDAO.java
@@ -3,6 +3,7 @@ package com.luisgmr.ifsc.hospital.dao;
 import com.luisgmr.ifsc.hospital.config.HibernateConfig;
 import com.luisgmr.ifsc.hospital.enums.PessoaType;
 import com.luisgmr.ifsc.hospital.model.Pessoa;
+import com.luisgmr.ifsc.hospital.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -12,22 +13,14 @@ import java.util.stream.Collectors;
 public class PessoaDAO {
 
     public void save(Pessoa pessoa) {
-        try (Session session = HibernateConfig.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.persist(pessoa);
-            transaction.commit();
-        } catch (Exception e) {
-            System.err.println("Erro ao salvar pessoa: " + e.getMessage());
-        }
+        HibernateUtil.save(pessoa);
     }
 
     public Pessoa findById(long id) {
-        try (Session session = HibernateConfig.getSessionFactory().openSession()) {
-            return session.get(Pessoa.class, id);
-        }
+        return HibernateUtil.findById(Pessoa.class, id);
     }
 
-    public List<Pessoa> findAll(PessoaType type) {
+    public List<Pessoa> findAllByType(PessoaType type) {
         try (Session session = HibernateConfig.getSessionFactory().openSession()) {
             return session.createQuery(
                     "FROM " + type.getClassObject().getSimpleName(),
@@ -37,5 +30,9 @@ public class PessoaDAO {
                     .map(pessoa -> (Pessoa) pessoa)
                     .collect(Collectors.toList());
         }
+    }
+
+    public void deleteById(long id) {
+        HibernateUtil.executeInTransaction(session -> session.delete(findById(id)));
     }
 }
---
+++ src/main/java/com/luisgmr/ifsc/hospital/utils/HibernateUtil.java
@@ -0,0 +1,47 @@
+package com.luisgmr.ifsc.hospital.utils;
+
+import com.luisgmr.ifsc.hospital.config.HibernateConfig;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class HibernateUtil {
+    public static void executeInTransaction(Consumer<Session> action) {
+        Transaction transaction = null;
+        try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            action.accept(session);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException("Erro na transação: " + e.getMessage(), e);
+        }
+    }
+
+    public static void save(Object entity) {
+        HibernateUtil.executeInTransaction(session -> session.persist(entity));
+    }
+
+    public static <T> T executeAndReturn(Function<Session, T> action) {
+        Transaction transaction = null;
+        try (Session session = HibernateConfig.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            T result = action.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw new RuntimeException("Erro na transação: " + e.getMessage(), e);
+        }
+    }
+
+    public static <T> T findById(Class<T> entityClass, Serializable id) {
+        return executeAndReturn(session -> session.get(entityClass, id));
+    }
+
+
+
+}
